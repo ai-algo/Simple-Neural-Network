86ae8e06187b0a6661454e9cb55185a9636e8d5b
ai-algo <30843772+ai-algo@users.noreply.github.com>
2017-09-30 17:41:02 +0100

    Update sigmoid-activation-function.py

--- sigmoid-activation-function.py
+++ sigmoid-activation-function.py
@@ -8,7 +8,7 @@ inputs = np.array([0.7, -0.3])
 weights = np.array([0.1, 0.8])
 bias = -0.1
 
-# TODO: Calculate the output
+# Calculate the output
 output = sigmoid(np.dot(weights, inputs) + bias)
 
 print(np.dot(weights, inputs))
